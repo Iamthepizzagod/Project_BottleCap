112ab60992833336de2b153bb97a5cd49474acb4
psmas <Psc123fE>
2021-03-19 02:57:36 -0400

    pcap import in progress

--- ip.py
+++ ip.py
@@ -4,9 +4,11 @@ from scapy.utils import RawPcapReader
 
 import pickle
 
+
 def ipCapture(numPackets):
     packets = sniff(count=numPackets)
     return packets
+
 
 def ip_src_dst(packets):  # Returns two lists, one of the packet capture's source and the other of the destination
     i = 0;
@@ -28,12 +30,29 @@ def print_src_dst(ipsrc, ipdst):  # spews out all IP sources and destinations fo
         i += 1
 
 
-def save_packets(numPackets):
+def save_packets_live(numPackets): #cpatures live packets and then saves them
     packets = sniff(count=numPackets)
-    print ("What would you like to name the file (extension will always be .obj)? ")
+    save_object(packets)
+
+def save_object(object): #saves a static object of any kind as a .obj
+    print("What would you like to name the file (extension will always be .obj)? ")
     filename = input()
     filename = str(filename) + ".obj"
     print("file name will be: " + filename)
     file_packets = open(filename, 'wb')
-    pickle.dump(packets, file_packets)
+    pickle.dump(object, file_packets)
 
+def pcapImport(pcapName):
+    option = "0"
+    # todo: add logic to make sure file + file name is a valid .pcap
+
+    packets = sniff(offline=pcapName)
+    packets[30].show() #debug line, dosent seem to work on different indicies...
+
+    while option != "n" and option != "y":
+        print("Would you like to save the pcap output to a pickle file?(y/n) ")
+        option = input()
+        if option == "y":
+            save_object(packets)
+
+    return packets
--- main.py
+++ main.py
@@ -15,8 +15,10 @@ def main():
 def build_parser():  # function that builds all the argument/parser lists
     parser = argparse.ArgumentParser(description='Test program for packet captures.')
     parser.add_argument("--echo", help="prints out the string you put in")
-    parser.add_argument("--packetCapture", help = "Capture x amount of packets and save them in a pickle object.", type = int)
+    parser.add_argument("--packetCapture", help="Capture x amount of packets and save them in a pickle object.",
+                        type=int)
     parser.add_argument("--printIP", help="Prints out source and destination IPs for x amount of IPs", type=int)
+    parser.add_argument("--pcapImport", help="Reads in a pcap file as a packet list.", type=str)
     return parser
 
 
@@ -31,19 +33,21 @@ def main_menu(program):
             packets = bottleIP.ipCapture(args.printIP)
             ip_src_dst = bottleIP.ip_src_dst(packets)
             bottleIP.print_src_dst(ip_src_dst[0], ip_src_dst[1])
+            packets[1].show()
 
-        if args.packetCapture is not None:
+        elif args.packetCapture is not None:
             bottleIP.save_packets(args.packetCapture)
 
-
-
-        if args.echo is not None:
+        elif args.echo is not None:
             print(args.echo)
+
+        elif args.pcapImport is not None:
+            print(args.pcapImport)
+            packets = bottleIP.pcapImport(args.pcapImport)
 
         while quitchar != "n" and quitchar != "y":
             print("Would you like to quit the program?(y/n) ")
             quitchar = input()
-            print(quitchar)
 
 
 if __name__ == '__main__':
